cde95c539f37b9a11af2d155d36b0e2a7ef9f957
LinkTo-ThePast <Luis_Rivera1007@hotmail.com>
2026-01-20 00:43:32 -0600

    wrong solution for merge sorted arrays

---
+++ src/lesson_05/MergeMain.java
@@ -0,0 +1,13 @@
+package lesson_05;
+
+import java.util.Arrays;
+
+public class MergeMain {
+    public static void main(String[] args) {
+        MergeSortedArrays mergeArrays = new MergeSortedArrays();
+        int[] nums1 = mergeArrays.nums1;
+        int[] nums2 = mergeArrays.nums2;
+
+        System.out.println(Arrays.toString(mergeArrays.getMergeOfArrays(nums1, nums2)));
+    }
+}
--- src/lesson_05/MergeSortedArrays.java
+++ src/lesson_05/MergeSortedArrays.java
@@ -4,15 +4,19 @@ public class MergeSortedArrays {
 
     // establish input examples
     // test case 1
-    final private int[] nums1 = {1, 2, 3, 0, 0, 0};
-    final private int[] nums2 = {2, 5, 6};
+    final public int[] nums1 = {1, 2, 3, 0, 0, 0};
+    final public int[] nums2 = {2, 5, 6};
 
-    public int[] getMergeOfArrays(int[] nums1, int m, int[] nums2, int n) {
-        m = nums1.length - nums2.length;
+    public int[] getMergeOfArrays(int[] nums1, int[] nums2) {
+        int m = nums1.length - nums2.length;
         //
         for (int i = m; i < nums1.length; i++) {
             for (int j = 0; j < nums2.length; j++) {
-                if (nums1[i] > nums2[j]) {
+                if (nums1[i-1] > nums2[j]) {
+                    int elementToSwap = nums1[i-1];
+                    nums1[i] = elementToSwap;
+                    nums1[i-1] = nums2[j];
+                } else {
                     nums1[i] = nums2[j];
                 }
             }
