7dfc4d48ccf4652be4fa9bb1f3bff8ddd9d7336f
LinkTo-ThePast <Luis_Rivera1007@hotmail.com>
2026-01-27 02:09:56 -0600

    chore(lesson 05): using backwards iteration to solve merge sorted arrays

--- src/lesson_05/MergeSortedArrays.java
+++ src/lesson_05/MergeSortedArrays.java
@@ -7,21 +7,9 @@ public class MergeSortedArrays {
     final public int[] nums1 = {1, 2, 3, 0, 0, 0};
     final public int[] nums2 = {2, 5, 6};
 
-    public int[] getMergeOfArrays(int[] nums1, int[] nums2) {
-        int m = nums1.length - nums2.length;
-        //
-        for (int i = m; i < nums1.length; i++) {
-            for (int j = 0; j < nums2.length; j++) {
-                if (nums1[i-1] > nums2[j]) {
-                    int elementToSwap = nums1[i-1];
-                    nums1[i] = elementToSwap;
-                    nums1[i-1] = nums2[j];
-                } else {
-                    nums1[i] = nums2[j];
-                }
-            }
-        }
-
-        return nums1;
+    public int getMergeOfArrays(int[] nums1, int[] nums2) {
+        // accesing last index and perform backwards iteration
+        int lastIndex = nums1.length - 1;
+        return  lastIndex;
     }
 }
