556af1fb17d18ad2316b0602ebc95a5fd610aaf7
LinkTo-ThePast <Luis_Rivera1007@hotmail.com>
2026-01-30 23:58:47 -0600

    lesson(05): try on merged sorted arrays

--- src/lesson_05/MergeSortedArrays.java
+++ src/lesson_05/MergeSortedArrays.java
@@ -7,9 +7,24 @@ public class MergeSortedArrays {
     final public int[] nums1 = {1, 2, 3, 0, 0, 0};
     final public int[] nums2 = {2, 5, 6};
 
-    public int getMergeOfArrays(int[] nums1, int[] nums2) {
-        // accesing last index and perform backwards iteration
-        int lastIndex = nums1.length - 1;
-        return  lastIndex;
+    public int[] getMergeOfArrays(int[] nums1, int[] nums2, int m, int n) {
+
+        //
+            for (int i = m - 1; i < nums1.length; i++) {
+                for (int j = 0; j < nums2.length; j++) {
+                    int lastItem = nums1[i]; // 3
+                    int comingItem = nums2[j]; // 5
+
+                    // check if lastItem is greater than coming item
+                    if (lastItem >= comingItem) {
+                        nums1[i + 1] = lastItem;
+                        nums1[i] = comingItem;
+                    } else {
+                        nums1[i] = lastItem;
+                        nums1[i+1] = comingItem;
+                    }
+                }
+            }
+        return nums1;
     }
 }
