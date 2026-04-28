d03a012872ec61623ed9567e5f1e1c4b8b4571bb
LinkTo-ThePast <Luis_Rivera1007@hotmail.com>
2026-04-28 00:52:07 -0600

    fixing binary search

--- src/lesson_07/BinarySearch.java
+++ src/lesson_07/BinarySearch.java
@@ -6,20 +6,6 @@ public class BinarySearch {
     int numberObjective;
 
     public int findNumber(int[] numbers, int numberObjective) {
-        if (numberObjective < 0 || numbers.length == 0) {
-            return 0;
-        }
 
-        int halfOfArray = numbers.length % 2;
-        int halfOfElements;
-        if (halfOfArray == 0) {
-            halfOfElements = numbers[halfOfArray];
-        } else {
-            halfOfElements = numbers[Math.floorDiv(numbers.length, 2)];
-        }
-
-        if (numberObjective < halfOfElements) {
-
-        }
     }
 }
