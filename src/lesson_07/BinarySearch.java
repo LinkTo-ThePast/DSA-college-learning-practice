68c1cc219f0a5f241144442a9d11bd321645582d
LinkTo-ThePast <Luis_Rivera1007@hotmail.com>
2026-04-28 00:59:13 -0600

    fixing binary search

--- src/lesson_07/BinarySearch.java
+++ src/lesson_07/BinarySearch.java
@@ -2,10 +2,17 @@ package lesson_07;
 
 public class BinarySearch {
 
-    int[] numbers;
-    int numberObjective;
+    /**
+     * Requirements for binary search: 1) a sorted array, 2) an item to find.
+     * */
+    int[] sorted_array;
+    int item;
 
-    public int findNumber(int[] numbers, int numberObjective) {
+    public int findNumber(int[] sorted_array, int item) {
+
+        if (sorted_array.length == 0) {
+            throw new IllegalArgumentException("The sorted arrays needs to have at least one item. The array length provided was: " + sorted_array.length);
+        }
 
     }
 }
