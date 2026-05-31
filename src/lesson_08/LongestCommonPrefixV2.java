f16a6679bc36b8bd589fb8ab966da3b5efe70cc9
LinkTo-ThePast <Luis_Rivera1007@hotmail.com>
2026-05-31 17:39:32 -0600

    relearning longest common prefrix

--- src/lesson_08/LongestCommonPrefixV2.java
+++ src/lesson_08/LongestCommonPrefixV2.java
@@ -2,6 +2,28 @@ package lesson_08;
 
 public class LongestCommonPrefixV2 {
 
+    // ONE INPUT
+    String[] strArray;
 
+    public string getCommonPrefix(String[] strArray)
+    {
+        // FIRST EDGE CASE DETECTED: to find a common sequence, we need more one item in the array
+        if (strArray.length <= 1) {
+            throw new IllegalArgumentException("Array of strings must contain at least two elements.");
+        }
 
+        // BEGIN WITH SOLUTION
+        // 1. SELECT ONE ARBITRARY string, in this case, the first one
+        for (int i = 0; i < strArray[0].length(); i++)
+        {
+            // 2. comparison between each subsequent character in the next strings
+            for (int s = 0; s < strArray.length; i++) {
+                if (strArray[0][i] =! strArray[i][s])
+                {
+
+                }
+            }
+        }
+
+    }
 }
