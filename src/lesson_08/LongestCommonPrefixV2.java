37bcc928fb0105478d8061757a4bdf1a4ef8db11
LinkTo-ThePast <Luis_Rivera1007@hotmail.com>
2026-05-31 20:59:46 -0600

    not understanding longest common prefix

--- src/lesson_08/LongestCommonPrefixV2.java
+++ src/lesson_08/LongestCommonPrefixV2.java
@@ -5,8 +5,13 @@ public class LongestCommonPrefixV2 {
     // ONE INPUT
     String[] strArray;
 
-    public string getCommonPrefix(String[] strArray)
+    public String getCommonPrefix(String[] strArray)
     {
+        // CONSTANTS
+        int stoppingIndex = 0;
+        boolean shouldStop = false;
+
+
         // FIRST EDGE CASE DETECTED: to find a common sequence, we need more one item in the array
         if (strArray.length <= 1) {
             throw new IllegalArgumentException("Array of strings must contain at least two elements.");
@@ -14,16 +19,32 @@ public class LongestCommonPrefixV2 {
 
         // BEGIN WITH SOLUTION
         // 1. SELECT ONE ARBITRARY string, in this case, the first one
-        for (int i = 0; i < strArray[0].length(); i++)
+        String selectedString = strArray[0];
+        // 2. ITERATE OVER EACH CHARACTER in that string
+        for (int c = 0; c < strArray[0].length(); c++)
         {
-            // 2. comparison between each subsequent character in the next strings
-            for (int s = 0; s < strArray.length; i++) {
-                if (strArray[0][i] =! strArray[i][s])
-                {
-
-                }
+            if (shouldStop)
+            {
+                break;
             }
-        }
 
+            // FIRST CYCLE: start to build a sequence of characters
+            char currentCharacter = selectedString.charAt(c);
+            // THEN, compare currentCharacter: is that character in other strings?
+            // 3. SELECT THE FIRST CHARACTER IN OTHER STRINGS
+            for (int s = 0; s < strArray.length; s++)
+            {
+                // IF current character is not equal to the comparing character, then is not part of the common PREFIX if there is one
+                if (strArray[0].charAt(c) != strArray[s].charAt(c)) {
+                    shouldStop = true;
+                    stoppingIndex -= 1;
+                    break;
+                }
+
+            }
+            // if the characters are EQUAL
+            stoppingIndex += 1;
+        }
+        return strArray[0].substring(0, stoppingIndex);
     }
 }
---
+++ src/lesson_08/Main.java
@@ -0,0 +1,14 @@
+package lesson_08;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        LongestCommonPrefixV2 getPrefix = new LongestCommonPrefixV2();
+
+        String[] words = {"car", "thalamus", "flight", "flower", "flow"};
+        String result = getPrefix.getCommonPrefix(words);
+        System.out.println("Common prefix is " + result);
+    }
+
+}
