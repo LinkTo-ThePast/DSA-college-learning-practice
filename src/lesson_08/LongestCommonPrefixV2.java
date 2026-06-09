360df086a3d202aa2892888e50d6ac4cc54ce0e3
LinkTo-ThePast <Luis_Rivera1007@hotmail.com>
2026-06-09 13:22:54 -0600

    final changes to longest common prefix

--- src/lesson_08/LongestCommonPrefixV2.java
+++ src/lesson_08/LongestCommonPrefixV2.java
@@ -4,21 +4,23 @@ public class LongestCommonPrefixV2 {
 
     private final String[] strs = {"flow", "flower", "flight"};
 
-    public String getLongestCommonPrefixV2(String[] strs)
+    public String getLongestCommonPrefix(String[] strs)
     {
-        // 1. extract the first character of an arbitrary string,
-        // ARBITRARY STRING = FIRST ONE
-        // COMPARE EVERY character of ARBITRARY STRING
-        String arbitraryString = strs[0];
-        for (int i = 0; i < arbitraryString.length(); i++) {
+        // select arbitrary string
+        // in this case, I will go with second ome
+        String arbitraryStr = strs[1];
+
+        // inner cycle: iterate over each character in our selected string
+        for (int i = 0; i < arbitraryStr.length(); i++)
+        {
             for (int j = 0; j < strs.length; j++)
             {
-                // IF THE CHARACTER IS THE SAME FOR EVERY OTHER STRING, STORE IT OR KEEP TRACK
-                    if ((strs[j].charAt(i) != arbitraryString.charAt(i)) || c >= strs[j].length())
-                    {
-                        break;
-                    }
+                if (i > strs[j].length() || (arbitraryStr.charAt(i) != strs[j].charAt(i))) {
+                    break;
+                }
             }
+            // extract common character
+
         }
     }
 
