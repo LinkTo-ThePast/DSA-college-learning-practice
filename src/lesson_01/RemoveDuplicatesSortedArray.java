068ff264d45cf60f7eac09acea21f7fc759ad722
LinkTo-ThePast <Luis_Rivera1007@hotmail.com>
2026-06-29 02:20:15 -0600

    chore: studying pointers

--- src/lesson_01/RemoveDuplicatesSortedArray.java
+++ src/lesson_01/RemoveDuplicatesSortedArray.java
@@ -6,20 +6,23 @@ public class RemoveDuplicatesSortedArray {
     // data structure used in this problem
     int[] nums = {1,1,1,2,2,2,3,3,3,4};
 
+    int k; // final K is going to represent total unique elements within the array
+
     public int getTotalUniqueElements(int[] nums)
     {
-        int[] uniqueNums = {};
+        // structure to study: pointer
+        int pointer = 1;
 
-        for (int i = 0; i < nums.length; i++)
+        for (int i = 1; i < nums.length; i++)
         {
-            int currentItem = nums[i];
-            int nextItem = nums[i+1];
+            // i = 1 -> 1 != 1 ? FALSE
+            // i = 3
+            if (nums[i] != nums[i-1]) {
 
-            if (currentItem != nextItem)
-            {
-
+                nums[pointer] = i;
             }
         }
+
 
         return 0;
     }
