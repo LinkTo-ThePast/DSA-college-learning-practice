60e8dbed9888d2d9ea46d454009ad3a0814ef4ed
LinkTo-ThePast <Luis_Rivera1007@hotmail.com>
2026-07-03 23:55:44 -0600

    chore: trying again binary search

--- src/lesson_07/BinarySearch.java
+++ src/lesson_07/BinarySearch.java
@@ -1,39 +1,13 @@
 package lesson_07;
 
 public class BinarySearch {
+    // data structure used in this problem
+    int[] nums;
+    int objective;
 
-    /**
-     * Requirements for binary search: 1) a sorted array, 2) an item to find.
-     * Returns found item index
-     * Returns -1 if the item was not found in the array.
-     * */
-    int[] sorted_array; // TODO: learn how to use encapsulation
-    int item;
-
-    public int findNumber(int[] sorted_array, int item) {
-
-        // let´s track initial state for indices
-        int initialPosition = 0;
-        int endingPositon = sorted_array.length - 1;
-
-        // TODO: investigate why this has log N complexity
-        while (initialPosition <= endingPositon) {
-
-            int middlePosition = Math.floorDiv((initialPosition + endingPositon), 2);
-            int guess = sorted_array[middlePosition];
-
-            if (guess == item) {
-                return middlePosition;
-            }
-
-            else if (guess < item) {
-                initialPosition = middlePosition + 1;
-            }
-            else {
-                endingPositon = middlePosition - 1;
-            }
-        }
-        // case when item was not found
-        return -1;
+    public int binarySearch(int[] nums, int objective) {
+        int left = 0;
+        int middle = Math.floorDiv(left+)
     }
+
 }
