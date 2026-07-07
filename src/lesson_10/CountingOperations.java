64087fc27710bfc14170a1962ff6d82437096af4
LinkTo-ThePast <Luis_Rivera1007@hotmail.com>
2026-07-07 01:43:28 -0600

    chore: counting fundamental operations

---
+++ src/lesson_10/CountingOperations.java
@@ -0,0 +1,15 @@
+package lesson_10;
+
+/**
+ * Lesson: running time considers NOT physical time, but fundamental operations
+ *  Let's make an example with simple or linear search within a non-decreasing sorted array
+ */
+public class CountingOperations {
+
+    int[] testingArray = {1,2,3,4,5,6,7};
+
+    public boolean containsNumber(int[] sortedArray, int objective) {
+        return false;
+    }
+
+}
