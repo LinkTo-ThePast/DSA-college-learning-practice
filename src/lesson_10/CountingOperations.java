8a974a8d93821efe124166bf1fd15eece0da8a38
LinkTo-ThePast <Luis_Rivera1007@hotmail.com>
2026-07-07 01:49:58 -0600

    chore: counting fundamental operations

--- src/lesson_10/CountingOperations.java
+++ src/lesson_10/CountingOperations.java
@@ -6,10 +6,31 @@ package lesson_10;
  */
 public class CountingOperations {
 
+    // 1 fundamental operation = variable assignation
     int[] testingArray = {1,2,3,4,5,6,7};
 
+    /**
+     *
+     * @param sortedArray
+     * @param objective
+     *  Fundamental operations:
+     *  variable assignation
+     *  arithmetic operation
+     *  calling a function
+     *  returning a function
+     *  boolean comparisons
+     *  accessing a structure
+     * @return True if the objective is found within the array
+     */
     public boolean containsNumber(int[] sortedArray, int objective) {
-        return false;
+
+        // two fundamental operations for calling this method: 2 variable assignation cases
+        for (int i = 0; i < sortedArray.length; i++)
+        // 1 variable assignation, 1 boolean comparison, one increment + one assignation = 4 fundamental operations to initialize the for cycle
+        {
+
+        }
+
     }
 
 }
