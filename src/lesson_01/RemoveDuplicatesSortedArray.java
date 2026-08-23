5aeb467ad8437b263d09ed5e8db4711b355fdede
LinkTo-ThePast <Luis_Rivera1007@hotmail.com>
2026-08-23 01:23:23 -0600

    chore(DSA): studying and recovering DSA study lessons

--- src/lesson_01/RemoveDuplicatesSortedArray.java
+++ src/lesson_01/RemoveDuplicatesSortedArray.java
@@ -6,11 +6,20 @@ public class RemoveDuplicatesSortedArray {
     /*
     *  OOP concepts: a class is a blueprint for objects. Objects can be instantiated from Classes
     * */
-    
-    int arr[];
+    /*
+    *  DS concepts: data structure studied in this problem ---> static array ---> solves: static sequence
+    * a static sequence does not increment sequence's size
+    * */
+    int nums[];
 
-    public int getTotalUniqueElements(int[] arr)
+    /**
+     * PUBLIC method: returns the total unique elements counts
+     * @param nums: static array in non-decreasing order
+     * @return total count for unique elements within the static array
+     */
+    public int getTotalUniqueElements(int[] nums)
     {
+        // safety
 
     }
 }
