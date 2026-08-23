9a3ea92f01651a52f1c0e08dc06e0a1bdcbfd875
LinkTo-ThePast <Luis_Rivera1007@hotmail.com>
2026-08-23 01:16:53 -0600

    chore: studying and recovering DSA study lessons

--- src/lesson_01/RemoveDuplicatesSortedArray.java
+++ src/lesson_01/RemoveDuplicatesSortedArray.java
@@ -1,40 +1,16 @@
 package lesson_01;
 
+
 public class RemoveDuplicatesSortedArray {
 
-    // properties
-    // consideration: sorted array, in this case, non-decreasing order
-    // data stucture used in this exercise: static array -> solves static sequence
-    int[] nums;
+    /*
+    *  OOP concepts: a class is a blueprint for objects. Objects can be instantiated from Classes
+    * */
+    
+    int arr[];
 
-    /**
-     * PUBLIC METHOD:
-     * @param nums: array of integers, sorted in non-decreasing order
-     * @return total count of unique elements within array
-     * CONSTRAINT: at least TWO element.
-     */
-    public int getTotalUniqueElements(int[] nums) {
+    public int getTotalUniqueElements(int[] arr)
+    {
 
-        // auxiliary properties
-        int uniqueElements;
-        int leftPointer = 1;
-
-        if (nums.length <= 1)
-        {
-            throw new IllegalArgumentException("Please, use arrays that at least contains two elements.");
-        }
-
-        for (int i = 1; i < nums.length; i++)
-        {
-            if (nums[i] != nums[i-1])
-            {
-                nums[leftPointer] = nums[i];
-                leftPointer++;
-            }
-        }
-        // the time we found a different item, and leftPointer was incremented is also equal the unique items we found within the array
-        uniqueElements = leftPointer;
-        return  uniqueElements;
     }
-
 }
