a54be4e2692906df340313261c11715f8422190e
LinkTo-ThePast <Luis_Rivera1007@hotmail.com>
2026-08-23 01:41:26 -0600

    chore(DSA): understanding as unique elements within the array

--- src/lesson_01/RemoveDuplicatesSortedArray.java
+++ src/lesson_01/RemoveDuplicatesSortedArray.java
@@ -14,12 +14,34 @@ public class RemoveDuplicatesSortedArray {
 
     /**
      * PUBLIC method: returns the total unique elements counts
-     * @param nums: static array in non-decreasing order
+     * @param nums: static array in non-decreasing order and AT least it needs to contain two items
      * @return total count for unique elements within the static array
      */
     public int getTotalUniqueElements(int[] nums)
     {
-        // safety
+        // store occasions where unique elements were found
+        int uniqueElements = 0;
 
+        // safety
+        if (nums.length < 2)
+        {
+            throw new IllegalArgumentException("Static array needs to have at least two elements.");
+        }
+
+        // 1. begin to iterate array
+        for (int i = 0; i < nums.length; i++)
+        {
+            if (i == nums.length - 1)
+            {
+                break;
+            }
+
+            if (nums[i] != nums[i+1])
+            {
+                uniqueElements++;
+            }
+        }
+
+        return uniqueElements + 1;
     }
 }
