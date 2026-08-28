16118243268082d0b39693e24285ee214af98d5c
LinkTo-ThePast <Luis_Rivera1007@hotmail.com>
2026-08-28 17:33:52 -0600

    chore: counting fundamental operations

--- src/lesson_01/RemoveDuplicatesSortedArray.java
+++ src/lesson_01/RemoveDuplicatesSortedArray.java
@@ -20,15 +20,22 @@ public class RemoveDuplicatesSortedArray {
     public int getTotalUniqueElements(int[] nums)
     {
         // store occasions where unique elements were found
+        // fundamental operation (1) => assign variable
         int uniqueElements = 0;
 
         // safety
+        // fundamental operation (2) => boolean comparison
         if (nums.length < 2)
         {
             throw new IllegalArgumentException("Static array needs to have at least two elements.");
         }
 
         // 1. begin to iterate array
+        // iterate over a static array, in the worst case scenario, takes N iterations
+        // fundamental operations = N operations as per item in the static array PLUS the respective operation made in that iteration, in this case, two boolean comparisons
+        // fundamental operation for an array of arr.length == 5, then N  x 3 respective operations inside each iteration = 15 fundamental operations
+        // in terms of big O notation, kg(n) being a asymptotic upper bound defined by this worst scenario, this algorithm is never going to take MORE running time
+        // that kg(n) of 15 fundamental operations?
         for (int i = 0; i < nums.length; i++)
         {
             if (i == nums.length - 1)
