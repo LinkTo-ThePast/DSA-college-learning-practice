ba094b7acfd4edccfbd8a7eae9118dd76c016b1c
LinkTo-ThePast <Luis_Rivera1007@hotmail.com>
2026-09-06 21:32:28 -0600

    chore(static array): solving with pointers in O(n)

--- src/lesson_01/Main.java
+++ src/lesson_01/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         // testing
-        int[] nums = {0,0,1,1,1,2,2,3,3,4};
+        int[] nums = {0,1,1,1,1,2,2,3,3,4};
 
         RemoveDuplicatesSortedArray getUniqueItems = new RemoveDuplicatesSortedArray();
         int uniqueItems = getUniqueItems.getTotalUniqueElements(nums);
--- src/lesson_01/RemoveDuplicatesSortedArray.java
+++ src/lesson_01/RemoveDuplicatesSortedArray.java
@@ -1,6 +1,5 @@
 package lesson_01;
 
-
 public class RemoveDuplicatesSortedArray {
 
     /*
@@ -10,6 +9,8 @@ public class RemoveDuplicatesSortedArray {
     *  DS concepts: data structure studied in this problem ---> static array ---> solves: static sequence
     * a static sequence does not increment sequence's size
     * */
+    // nums example -> [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
+    // output should be -> k (different elements) = 5, and the modified array in-place [0, 1, 2, 3, 4, _, _, _, _, _]
     int nums[];
 
     /**
@@ -21,7 +22,7 @@ public class RemoveDuplicatesSortedArray {
     {
         // store occasions where unique elements were found
         // fundamental operation (1) => assign variable
-        int uniqueElements = 0;
+        //int uniqueElements = 0;
 
         // safety
         // fundamental operation (2) => boolean comparison
@@ -30,25 +31,32 @@ public class RemoveDuplicatesSortedArray {
             throw new IllegalArgumentException("Static array needs to have at least two elements.");
         }
 
+        // check the non-decreasing sorted condition
+        // counting fundamental operations => boolean comparison
+        // accessing to a memory address where array is allocated is constant time according to our model of computation studied in the previous class
+        if (nums[1] < nums[0])
+        {
+            throw new IllegalArgumentException("The input array must be sorted in non-decreasing order.");
+        }
+
+
+        int leftPointer = 1;
+
         // 1. begin to iterate array
         // iterate over a static array, in the worst case scenario, takes N iterations
         // fundamental operations = N operations as per item in the static array PLUS the respective operation made in that iteration, in this case, two boolean comparisons
         // fundamental operation for an array of arr.length == 5, then N  x 3 respective operations inside each iteration = 15 fundamental operations
         // in terms of big O notation, kg(n) being a asymptotic upper bound defined by this worst scenario, this algorithm is never going to take MORE running time
         // that kg(n) of 15 fundamental operations?
-        for (int i = 0; i < nums.length; i++)
+        for (int i = 1; i < nums.length; i++)
         {
-            if (i == nums.length - 1)
+            if (nums[i] != nums[i-1])
             {
-                break;
-            }
-
-            if (nums[i] != nums[i+1])
-            {
-                uniqueElements++;
+                nums[leftPointer] = nums[i];
+                leftPointer++;
             }
         }
 
-        return uniqueElements + 1;
+        return leftPointer;
     }
 }
